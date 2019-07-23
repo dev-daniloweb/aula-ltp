fa8bbb6214584f8163401a67a859c54e56df0334
dev-daniloweb <dev.daniloweb@gmail.com>
2019-07-23 15:55:36 -0300

    Alteração nos STATUS_CODE

--- src/app/controllers/clientController.js
+++ src/app/controllers/clientController.js
@@ -22,7 +22,7 @@ module.exports = {
             
             res.status(200).json(client);
         } catch (error) {
-            res.status(400).json({ menssage: error.message });
+            res.status(400).json({ error: error.message });
         }        
     },
     
@@ -33,10 +33,10 @@ module.exports = {
             if(client !== null){
                 res.status(200).json(client);
             } else {
-                res.status(404).json({ message: "Client not exist" });
+                res.status(404).json({ error: "Client not exist" });
             }
         } catch (error) {
-            res.status(404).json({ message: "Invalid id" });
+            res.status(400).json({ error: "Invalid id" });
         }        
     },
 
@@ -54,11 +54,11 @@ module.exports = {
             if(client !== null){
                 res.json({ message: "Client successfully updated" });
             } else {
-                res.status(404).json({ message: "Client not exist" });
+                res.status(404).json({ error: "Client not exist" });
             }
 
         } catch (error) {
-            res.status(404).json({ message: "Invalid id" });
+            res.status(400).json({ error: "Invalid id" });
         }
     },
 
@@ -68,10 +68,10 @@ module.exports = {
             if(client !== null){
                 res.json({ message: "Client successfully removed" });
             } else {
-                res.status(404).json({ message: "Client not exist" });
+                res.status(404).json({ error: "Client not exist" });
             }
         } catch(error) {
-            res.status(404).json({ message: "Invalid id" });
+            res.status(400).json({ error: "Invalid id" });
         }
     },
 };
